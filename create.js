7be7177aaa8be8e2f6f64f5be465357557ad210a
Franklin Yin <yin4thewin@gmail.com>
2022-04-28 08:23:00 +0000

    Add some comments as documentation

--- create.js
+++ create.js
@@ -2,8 +2,11 @@ const { exec } = require("child_process");
 const fs = require('fs')
 
 module.exports={
+    //Creating proper directory for bot
     createBot(clientId, botToken, username, projectName){
+        //Generate URL for your project
         const dest = "../bots/"+username+"/"+projectName
+        //Run the necessary commands to create bot: namely, create new directory, then clone setup files from a base folder into new folder
         return exec("mkdir "+dest+" && cp ../base/index.js "+dest+"&& cp ../base/deploy-commands.js "+dest+" && touch "+dest+"/afkusers.json "+dest+"/remindusers.json "+dest+"/database.json "+dest+"/listeners.json ", (error, stdout, stderr) => {
             if (error) {
                 return;
@@ -11,6 +14,7 @@ module.exports={
             if (stderr) {
                 return;
             }
+            //If successful, add bot's config info to config.json
             fs.appendFileSync(dest+'/config.json', JSON.stringify({
                 clientId: clientId,
                 token: botToken,
@@ -21,8 +25,11 @@ module.exports={
             }))
         })
     },
+    //Spawning a new process for your bot
     runBot(username, projectName){
+        //Again, generting URL where your bot lives
         const dest = "../bots/"+username+"/"+projectName
+        //Deploying commands and starting your bot
         return exec("node "+dest+"/deploy-commands.js && pm2 start "+dest+"/index.js", (error, stdout, stderr) => {
             if (error) {
                 console.log("err", stderr)
--- server.js
+++ server.js
@@ -9,19 +9,27 @@ app.use(cors())
 app.use(express.json());
 
 app.post('/birth/', (req, res) => {
+    //Reading body of POST request: get bot token, username, and desired project name
     const botToken = req.body.botToken
     const username = req.body.username
     const projectName = req.body.projectName
+
+    //Ensure fields aren't empty
     if(!(botToken&&username&&projectName))
         res.status(500).send({ error: 'Missing fields' })
     else{
+        //If fields nonempty, attempt to login with provided bot token to check if valid (then immediately logout!)
         const client = new Client({ intents: [] });
         client.login(botToken).then(()=>{
+            //Get client ID
             const clientId=client.user.id
+            //Immediately logout!
             client.destroy()
+            //Attempt to create new directory for your bot
             const create = createBot(clientId, botToken, username, projectName)
             create.on('close', (code)=>{
                 if(code==0){
+                    //If successful, try to run the newly created bot!
                     const run = runBot(username, projectName)
                     run.on('close', (code, signal)=>{
                         console.log(code, signal)
@@ -29,9 +37,11 @@ app.post('/birth/', (req, res) => {
                     res.json(req.body)
                 }
                 else 
+                //If unsuccessful, project probably already exists on our file system
                     res.status(500).send({ error: 'dir already exists' })
             })
         }).catch((err)=>{
+            //Bot token provided was invalid
             console.log(err)
             client.destroy()
             res.status(500).send({ error: 'invalid token' })
