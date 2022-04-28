d0a81de5d9933a1e627b21c2008f790ebcab72bb
Franklin Yin <yin4thewin@gmail.com>
2022-04-28 07:03:18 +0000

    hopefully i didnt commit node modules

---
+++ create.js
@@ -0,0 +1,37 @@
+const { exec } = require("child_process");
+const fs = require('fs')
+
+module.exports={
+    createBot(clientId, botToken, username, projectName){
+        const dest = "../bots/"+username+"/"+projectName
+        return exec("mkdir "+dest+" && cp ../base/index.js "+dest+"&& cp ../base/deploy-commands.js "+dest+" && touch "+dest+"/afkusers.json "+dest+"/remindusers.json "+dest+"/database.json "+dest+"/listeners.json ", (error, stdout, stderr) => {
+            if (error) {
+                return;
+            }
+            if (stderr) {
+                return;
+            }
+            fs.appendFileSync(dest+'/config.json', JSON.stringify({
+                clientId: clientId,
+                token: botToken,
+                rootDir: "../../"+dest,
+                ticketsEnabled: true, 
+                modsEnabled: true, 
+                musicEnabled: true
+            }))
+            console.log(`stdout: ${stdout}`);
+        })
+    },
+    runBot(username, projectName){
+        const dest = "../bots/"+username+"/"+projectName
+        return exec("node "+dest+"/deploy-commands.js && pm2 start "+dest+"/index.js", (error, stdout, stderr) => {
+            if (error) {
+                console.log("err", stderr)
+                return;
+            }
+            if (stderr) {
+                return;
+            }
+        })
+    }
+}
---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const { createBot, runBot } = require('./create');
+var cors = require('cors')
+const app = express()
+const port = 3000
+
+app.use(cors())
+app.use(express.json());
+
+app.post('/birth/', (req, res) => {
+    const clientId = req.body.clientId
+    const botToken = req.body.botToken
+    const username = req.body.username
+    const projectName = req.body.projectName
+    if(!(clientId&&botToken&&username&&projectName))
+        res.status(500).send({ error: 'Missing fields' })
+    else{
+        const create = createBot(clientId, botToken, username, projectName)
+        create.on('close', (code)=>{
+            if(code==0){
+                const run = runBot(username, projectName)
+                run.on('close', (code, signal)=>{
+                    console.log(code, signal)
+                })
+                res.json(req.body)
+            }
+            else 
+                res.status(500).send({ error: 'Missing fields' })
+        })
+    }
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
